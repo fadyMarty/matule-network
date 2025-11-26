f1bceb5c29b5bd7c6969d43cb785821e38552eb0
fadyMarty <antoshka1234098@gmail.com>
2025-11-26 04:37:36 -0800

    Update version of library

--- network/build.gradle.kts
+++ network/build.gradle.kts
@@ -59,7 +59,7 @@ publishing {
         register<MavenPublication>("release") {
             groupId = "com.github.fadymarty"
             artifactId = "matule-network"
-            version = "1.0.0"
+            version = "1.0.1"
 
             afterEvaluate {
                 from(components["release"])
